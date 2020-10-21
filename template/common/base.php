d7a1dfceaac624741c99d32144d38921c3e46f39
Pr0digal <heljhumenad@gmail.com>
2020-10-21 15:51:20 +0800

    add fontawesome for fonts and icons

--- template/common/base.php
+++ template/common/base.php
@@ -5,10 +5,11 @@ include('config/config.php');
 ?>
 <html>
 <head>
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <!-- <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"> -->
     <link href="static/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link href="static/bootstrap/css/fontawesome.min.css" rel="stylesheet">
+    <!-- <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script> -->
+    <!-- <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
 <body>
     <title>User Login</title>
     <div class="container">
