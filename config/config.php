19a89ce2135283aa202bbe397f2092a01c16033f
Heljhum Enad <heljhumenad@gmail.com>
2020-10-22 16:21:48 +0800

    add opp style for connection of db

--- config/config.php
+++ config/config.php
@@ -15,3 +15,53 @@ if($db_connection->error) {
 } 
 
 ?>
+
+
+<?php 
+
+class Connect {
+
+    // connect this using instantiate the class
+    // and connect like this Connect($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DATABASE_NAME)
+
+    public function __construct($server, $username, $password, $db_name) {
+        $this->server = $server;
+        $this->username = $username;
+        $this->password = $password;
+        $this->db_name = $db_name;
+        $this->connect();
+    }
+
+    public function message($messag) {
+
+        return $message;
+    }
+    // create connection through mysqli
+    public function connect() {
+
+        $connection = new mysqli($this->server, 
+                                 $this->username, 
+                                 $this->password, 
+                                 $this->db_name);
+
+        if($connection->error) { 
+            $this->message('Connection Error'); 
+                return mysqli_connect_errno();
+        }
+    }
+    // close connection
+    public function close() {
+        return mysqli_close();
+    }
+
+    public function get_first_row($sql) {
+        $results = $this->connect()->query($sql);
+        while($row = $results->fetch_row()){
+            echo($row); //print row
+        }
+        $result->close();
+    $this->close();
+    }
+}
+
+?>
